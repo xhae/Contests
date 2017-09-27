a9926755ac2620ce1102523d7902c57917be86ec
Hyeonjong Ryu <xoancer@gmail.com>
2017-09-27 14:32:56 +0900

    Dinic flow 0 return bug modified & enhancements

--- algorithms/dinic.cpp
+++ algorithms/dinic.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,21 +8,20 @@ class Dinic {
 private:
     struct Edge {
         long long c, f;
-        int dest;
-        Edge *inv;
+        int dest, inv;
     };
 
     vector<vector<Edge>> edge;
     vector<int> level;
-    int s, e;
+    int src, sink;
     const long long INF = 1000000000;
 
     bool leveling() {
-        level = vector<int>(edge.size(), -1);
-        level[s] = 0;
+        fill(level.begin(), level.end(), -1);
+        level[src] = 0;
 
         queue<int> q;
-        q.push(s);
+        q.push(src);
         while(!q.empty()) {
             int u = q.front();
             q.pop();
@@ -32,37 +32,37 @@ private:
                 }
         }
         
-        return level[e] != -1;
+        return level[sink] != -1;
     }
 
     long long fillAugpath(int u, long long have) {
-        if(u == e) return have;
+        if(u == sink) return have;
 
         for(auto &e: edge[u])
             if(level[e.dest] == level[u] + 1 and e.c - e.f > 0) {
                 auto f = fillAugpath(e.dest, min(have, e.c - e.f));
-                e.f += f;
-                e.inv->f -= f;
-                return f;
+                if(f > 0) {
+                    e.f += f;
+                    edge[e.dest][e.inv].f -= f;
+                    return f;
+                }
             }
 
         return 0ll;
     }
 
 public:
-    Dinic(int n, int _s, int _e): s(_s), e(_e) { edge.resize(n); }
+    Dinic(int n, int _s, int _e): src(_s), sink(_e) { edge.resize(n); level.resize(n); }
 
     // Should be called once for unordered (u, v) pair. Unify them before you call in case of multiple edges.
     void addEdge(int u, int v, long long c, long long revc = 0) {
-        edge[u].push_back({c, 0ll, v, NULL});
-        edge[v].push_back({revc, 0ll, u, NULL});
-        edge[u].back().inv = &edge[v].back();
-        edge[v].back().inv = &edge[u].back();
+        edge[u].push_back({c, 0ll, v, (int)edge[v].size()});
+        edge[v].push_back({revc, 0ll, u, (int)edge[u].size() - 1});
     }
 
     long long flow() {
         long long ret = 0, res;
-        while(leveling()) while(res = fillAugpath(s, INF), ret += res, res);
+        while(leveling()) while(res = fillAugpath(src, INF), ret += res, res != 0ll);
         return ret;
     }
 };
